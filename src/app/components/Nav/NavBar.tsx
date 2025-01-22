1a2adac5a8c5ce8cc0e4d6287073ca177e9b482c
suprajasribalaji <suprajasri.balaji@gmail.com>
2025-01-22 16:31:57 +0530

    page updated

--- src/app/components/Nav/NavBar.tsx
+++ src/app/components/Nav/NavBar.tsx
@@ -65,7 +65,7 @@ const NavBar = () => {
                     <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <NavBarList
                             href="/"
-                            className="block py-2 px-3 text-white bg-darOliveGreen rounded md:bg-transparent md:text-darkOliveGreen md:p-0 md:dark:text-darkOliveGreen text-[96%] font-[Arial]"
+                            className="block py-2 px-3 rounded md:bg-transparent md:hover:text-oliveGreen md:text-black md:p-0 md:dark:text-darkOliveGreen text-[96%] font-[Arial]"
                             name="Menu"
                         />
                         <NavBarList
