7516c8bd5a60521356cf419807c833ca245d6e33
suprajasribalaji <suprajasri.balaji@gmail.com>
2025-01-22 16:03:41 +0530

    removed unused declaraions in main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import Footer from "./components/Footer/Footer";
 import NavBar from "./components/Nav/NavBar";
 
 const Home = () => {
-  let year = new Date().getFullYear();
-  let router = useRouter();
   return (
     <>
       <NavBar />
