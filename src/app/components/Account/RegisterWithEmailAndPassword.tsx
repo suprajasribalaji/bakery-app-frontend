97181ebd459aa09bc260df5930d5c82ed63be43b
suprajasribalaji <suprajasri.balaji@gmail.com>
2025-01-28 23:53:59 +0530

    Simple login and signup logic for backend is completed

--- src/app/components/Account/RegisterWithEmailAndPassword.tsx
+++ src/app/components/Account/RegisterWithEmailAndPassword.tsx
@@ -1,26 +1,126 @@
+'use client'
+
 import React, { useState } from "react";
 import Button from "../Button/Button";
 import { RegisterWithEmailAndPasswordProps } from "@/utils/types";
 import ForgotPasswordModal from "../Modal/ForgotPasswordModal";
+import { validateEmail, validatePassword } from "@/helpers/validators";
+import axios from "axios";
+import { message } from "antd";
+import { useRouter } from "next/navigation";
 
 const RegisterWithEmailAndPassword: React.FC<RegisterWithEmailAndPasswordProps> = (props) => {
     const { pageName } = props;
+    const router = useRouter();
     const [isForgotPasswordModalOpened, setIsForgotPasswordModalOpened] = useState<boolean>(false);
+    const [username, setUsername] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [emailError, setEmailError] = useState<string | null>(null);
+    const [passwordError, setPasswordError] = useState<string | null>(null);   
 
+    const [messageApi, contextHolder] = message.useMessage();
+
+    const success = (message: string) => {
+      messageApi.open({
+        type: 'success',
+        content: message,
+      });
+    };
+  
     const handleForgotPasswordModal = () => {
         setIsForgotPasswordModalOpened(true);
     };
 
+    const handleLoginOrSignup = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setEmailError(validateEmail(email));
+        setPasswordError(validatePassword(password));
+
+        if (!emailError && !passwordError) {
+            if (pageName === "Sign Up") {
+                try {
+                    const response = await axios.post('http://127.0.0.1:8000/api/accounts/signup/', {
+                        username: username,
+                        email: email,
+                        password: password,
+                    });
+                    if(response.status===201){
+                        setUsername('');
+                        setEmail('');
+                        setPassword('');
+                        setEmailError('');
+                        setPasswordError('');
+                        router.push('/');
+                        console.log('User created successfully');
+                        success('User Created Successfully');
+                    }
+                } catch (error: any) {
+                    console.log(error, ' ---____---');
+                    message.error('Something went wrong. Please try again later');
+                }
+            }
+
+            if (pageName === "Log In") {
+                try {
+                    const response = await axios.post('http://127.0.0.1:8000/api/accounts/login/', {
+                        email: email,
+                        password: password,
+                    });
+                    if(response.status===200){
+                        setEmail('');
+                        setPassword('');
+                        setEmailError('');
+                        setPasswordError('');
+                        router.push('/');
+                        console.log('User Logged in successfully');
+                        success('User Logged in successfully');
+                    }
+                } catch (error: any) {
+                    console.log(error, ' ---____---');
+                    message.error('Something went wrong. Please try again later');
+                }
+            }
+        }
+    };
+
     return (
         <div>
-            <form className="w-[90%] max-w-md mx-auto flex flex-col items-center">
+            {contextHolder}
+            <form className="w-[90%] max-w-md mx-auto flex flex-col items-center" onSubmit={handleLoginOrSignup}>
+                {
+                    pageName==="Sign Up" && 
+                    <div className="relative z-0 w-[85%] mb-7 group">
+                        <input
+                            type="username"
+                            name="floating_username"
+                            id="floating_username"
+                            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none peer"
+                            placeholder=" "
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            required
+                        />
+                        <label
+                            htmlFor="floating_username"
+                            className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+                        >
+                            Username
+                        </label>
+                    </div>
+                }
                 <div className="relative z-0 w-[85%] mb-7 group">
                     <input
                         type="email"
                         name="floating_email"
                         id="floating_email"
-                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none peer"
+                        className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 ${
+                            emailError ? "border-red-500" : "border-gray-300"
+                        } appearance-none focus:outline-none peer`}
                         placeholder=" "
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        onBlur={() => validateEmail(email)}
                         required
                     />
                     <label
@@ -29,6 +129,7 @@ const RegisterWithEmailAndPassword: React.FC<RegisterWithEmailAndPasswordProps> 
                     >
                         Email
                     </label>
+                    {emailError && <span className="text-red-500 text-xs mt-1">{emailError}</span>}
                 </div>
 
                 <div className="relative z-0 w-[85%] mb-6 group">
@@ -36,8 +137,13 @@ const RegisterWithEmailAndPassword: React.FC<RegisterWithEmailAndPasswordProps> 
                         type="password"
                         name="floating_password"
                         id="floating_password"
-                        className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none peer"
+                        className={`block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 ${
+                            passwordError ? "border-red-500" : "border-gray-300"
+                        } appearance-none focus:outline-none peer`}
                         placeholder=" "
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        onBlur={() => validatePassword(password)}
                         required
                     />
                     <label
@@ -46,6 +152,7 @@ const RegisterWithEmailAndPassword: React.FC<RegisterWithEmailAndPasswordProps> 
                     >
                         Password
                     </label>
+                    {passwordError && <span className="text-red-500 text-xs mt-1">{passwordError}</span>}
                 </div>
 
                 {pageName === "Sign Up" ? (
@@ -67,29 +174,13 @@ const RegisterWithEmailAndPassword: React.FC<RegisterWithEmailAndPasswordProps> 
                         <span className="text-center">{pageName}</span>
                     </button>
                 </div>
-
-                <div className="mb-6">
-                    <span className="text-center text-gray-500">or</span>
-                </div>
-
-                <div className="flex justify-center gap-8">
-                    <Button
-                        path="/"
-                        iconUrl="/socialLink/google.png"
-                        altText="Google Icon"
-                        className="w-6 h-6"
-                    />
-                    <Button
-                        path="/"
-                        iconUrl="/socialLink/blue-facebook.png"
-                        altText="Facebook Icon"
-                        className="w-6 h-6"
-                    />
-                </div>
             </form>
 
             {isForgotPasswordModalOpened && (
-                <ForgotPasswordModal isModalOpen={isForgotPasswordModalOpened} setIsModalOpen={setIsForgotPasswordModalOpened}  />
+                <ForgotPasswordModal
+                    isModalOpen={isForgotPasswordModalOpened}
+                    setIsModalOpen={setIsForgotPasswordModalOpened}
+                />
             )}
         </div>
     );
---
+++ src/helpers/validators.tsx
@@ -0,0 +1,17 @@
+import { emailRegex, passwordRegex } from "@/utils/regex";
+
+export const validateEmail = (email: string) => {
+    if (!emailRegex.test(email)) {
+        return "Invalid email format";
+    } else {
+        return null;
+    }
+};
+
+export const validatePassword = (password: string) => {
+    if (!passwordRegex.test(password)) {
+        return "Password must contain at least 8 characters, 1 uppercase, 1 number, and 1 lowercase.";
+    } else {
+        return null;
+    }
+};
